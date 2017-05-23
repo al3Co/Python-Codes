66631bd323381ac3f33d28c6d6ac43f07bbe719f
Aldo Contreras <aldocontreras@Aldos-MacBook-Air.local>
2017-05-23 18:08:39 +0200

    Mod

--- e3_Exercise.py
+++ e3_Exercise.py
@@ -5,9 +5,9 @@ def raquel2():      #Imprimir x veces hola (Pedir al usuario numero de veces)
 
 def raquel3():      #Imprimir promedio de 4 materias diferentes
 
-def raquel3():      #Promedio de 4 materias de 2 Alumnos
+def raquel4():      #Promedio de 4 materias de 2 Alumnos
 
-def raquel4():      #Promedio de X alumnos y Y materia, verificando errores de escritura
+def raquel5():      #Promedio de X alumnos y Y materia, verificando errores de escritura
 
 if __name__ == "__main__":
     raquel1()
