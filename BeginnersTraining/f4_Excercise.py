16e263c0a8cc3e398115f43e79f512bc65c9b881
Aldo Contreras <aldocontreras@Aldos-MacBook-Air.local>
2017-05-29 09:54:27 +0200

    Dictionary added

--- BeginnersTraining/f4_Excercise.py
+++ BeginnersTraining/f4_Excercise.py
@@ -1,4 +1,5 @@
-# Ask for 5 names and save in an array (Just Strings)
+# Ask for 5 names and save them in an array (Just Strings)
+# Ask for 5 names and save them in a dictionary (Just Strings)
 # Ask for ages of each saved name (Just Integers)
 # Create function to evaluate the average age
 # Create function to print Name and Age of each member
